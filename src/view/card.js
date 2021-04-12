77dbc60b1d5b8b629a7d37aa367cfc169f00acef
PavelCraze <54514757+PavelCraze@users.noreply.github.com>
2021-04-13 01:31:29 +0300

    WIP

--- src/view/card.js
+++ src/view/card.js
@@ -1,8 +1,8 @@
 import {createElement} from "../util.js";
 
 export default class Card {
+
   constructor({title, rating, date, duration, genreNames, poster, description, comments}) {
-    this._element = null;
     this._title = title;
     this._rating = rating;
     this._date = date;
@@ -11,29 +11,30 @@ export default class Card {
     this._poster = poster;
     this._description = description;
     this._comments = comments;
+
+    this._element = null;
+
   }
+
   getTemplate() {
-
-
     return (
       `<article class="film-card">
-            <h3 class="film-card__title">${this._title}</h3>
-            <p class="film-card__rating">${this._rating}</p>
-            <p class="film-card__info">
-              <span class="film-card__year">${this._date.getFullYear()}</span>
-              <span class="film-card__duration">${this._duration}</span>
-              <span class="film-card__genre">${this._genreNames[0]}</span>
-            </p>
-            <img src="./images/posters/${this._poster}" alt="${this._title}" class="film-card__poster">
-            <p class="film-card__description">${this._description}</p>
-            <a class="film-card__comments">${this._comments.length} comments</a>
-            <form class="film-card__controls">
-              <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-              <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-              <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-            </form>
-          </article>`
-    );
+      <h3 class="film-card__title">${this._title}</h3>
+      <p class="film-card__rating">${this._rating}</p>
+      <p class="film-card__info">
+      <span class="film-card__year">${this._date.getFullYear()}</span>
+        <span class="film-card__duration">${this._duration}</span>
+        <span class="film-card__genre">${this._genreNames[0]}</span>
+      </p>
+      <img src="./images/posters/${this._poster}" alt="${this._title}" class="film-card__poster">
+      <p class="film-card__description">${this._description}</p>
+      <a class="film-card__comments">${this._comments.length} comments</a>
+      <form class="film-card__controls">
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+      </form>
+  </article>`);
   }
 
   getElement() {
@@ -48,3 +49,4 @@ export default class Card {
     this._element = null;
   }
 }
+
